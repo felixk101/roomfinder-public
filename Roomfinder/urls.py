4b1c7e980bb21a0a6ccde5d993c33db13c9a7d44
Felix Kampfer <felix.kampfer@hs-augsburg.de>
2017-10-26 01:53:55 +0200

    added webuntis query, changed route for index

--- Roomfinder/urls.py
+++ Roomfinder/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^RoomfinderApp/', include('RoomfinderApp.urls'))
+    url(r'', include('RoomfinderApp.urls'))
 ]
--- RoomfinderApp/views.py
+++ RoomfinderApp/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import webuntis
+import datetime
+import secret
+
 
 # Create your views here.
 def index(request):
+    s = webuntis.Session(
+        username=secret.username,
+        password=secret.password,
+        server='https://melpomene.webuntis.com',
+        school='HS-augsburg',
+        useragent='HSARoomfinder App (felix.kampfer@hs-augsburg.de)'
+    ).login()
+
+    today = datetime.date.today()
+    thursday = today - datetime.timedelta(days=today.weekday())
+    print(str(datetime.timedelta(days=today.weekday())))
+    monday = thursday + datetime.timedelta(days=4)
+
+    room = s.rooms().filter(id=74)[0]  # room m1.01
+    tt = s.timetable(room=room, start=monday, end=monday)
+    for row in tt:
+        print('from ' + str(row.start) + ' until '+str(row.end) + '(type = '+row.type+'):')
+        for subject in row.subjects:
+            print('  '+subject.long_name)
+        for klasse in row.klassen:
+            print('  attended by: '+klasse.long_name)
+    s.logout()
     return HttpResponse("Hello World")
